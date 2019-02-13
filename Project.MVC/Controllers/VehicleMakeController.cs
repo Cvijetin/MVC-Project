a122f0801def1df434659f8d9f15e5595a87053b
Cvijetin <vidakovic.cvijetin@gmail.com>
2019-02-13 18:02:01 +0100

    DAL

--- Project.MVC/Controllers/VehicleMakeController.cs
+++ Project.MVC/Controllers/VehicleMakeController.cs
@@ -1,12 +1,9 @@
-﻿using Project.Service;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
-using Project.Service.Implementation;
-using Project.Service.Interfaces;
 using System.Threading.Tasks;
 
 namespace Project.MVC.Controllers
@@ -15,47 +12,16 @@ namespace Project.MVC.Controllers
     public class VehicleMakeController : Controller
     {
       
-        private IVehicleMakeService _vehicleMakeService;
-        private IArrange _arrange;
         
-        public VehicleMakeController(IVehicleMakeService vehicleMakeService, IArrange arrange)
+        
+        public VehicleMakeController()
         {
-            _vehicleMakeService = vehicleMakeService;
-            _arrange = arrange;
+           
         }
               
-        public async Task<ActionResult> Index(string sortOrder, string searchString, string currentFilter, int? page)
+        public ActionResult Index()
         {
-            try
-            {
-                ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-                ViewBag.AbrvParm = sortOrder == "Abrv" ? "abrv_desc" : "Abrv";
-                ViewBag.CurrentSort = sortOrder;
-
-                if (searchString != null)
-                {
-                    page = 1;
-                }
-                else
-                {
-                    searchString = currentFilter;
-                }
-
-                ViewBag.CurrentFilter = searchString;
-
-                _arrange.PageSize = 4;
-                _arrange.Page = page;
-                _arrange.SearchString = searchString;
-                _arrange.SortOrder = sortOrder;
-
-                var pagedList = await _vehicleMakeService.GetPagedListAsync(_arrange);
-
-                return View(pagedList);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            return View();
 
         }
         
